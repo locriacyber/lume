3b3c08affbb23a87c3e12d4d26cdb2e3d2506f4e
Giles Dring <giles@dringtech.com>
2024-04-10 15:09:51 +0100

    Correctly handle document fragments
    Linkedom, unline deno-dom, doesn't add in the html / head / body wrapper
    if they don't exist.
    This interferes with a operation of a number of plugins.
    
    This commit fixes this behaviour and alters tests with trivial
    whitespace changes to snapshots.

--- core/utils/dom.ts
+++ core/utils/dom.ts
@@ -19,7 +19,26 @@ export function documentToString(document: Document) {
 
 /** Parse a string with HTML code and return a Document */
 export function stringToDocument(string: string): Document {
-  const document = parser.parseFromString(string, "text/html");
+  // Get the DOCTYPE and rest of the document
+  const [_match, documentType, documentString] = Array.from(
+    string.match(/^(<!DOCTYPE .*?>){0,1}(.*)$/si)!,
+  );
+
+  let htmlSource = string;
+
+  // If it's HTML or missing DOCTYPE, but not an HTML document node...
+  if (
+    (
+      !documentType ||
+      (documentType && documentType.match(/^<!DOCTYPE html>$/i))
+    ) && !documentString.match(/^\s*<html/is)
+  ) {
+    htmlSource = `${
+      documentType || ""
+    }\n<html><head></head><body>${documentString}</body></html>`;
+  }
+
+  const document = parser.parseFromString(htmlSource, "text/html");
 
   if (!document) {
     throw new Error("Unable to parse the HTML code");

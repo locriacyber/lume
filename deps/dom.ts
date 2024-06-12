470d215ff55a5fb4faf0be26910644aadc8465c2
iacore <noreply+gpg-stub@1a-insec.net>
2024-06-12 09:17:12 +0000

    a

--- deps/dom.ts
+++ deps/dom.ts
@@ -1,4 +1,4 @@
 /// <reference lib="dom" />
 /// <reference lib="dom.iterable" />
 // export * from "https://deno.land/x/deno_dom@v0.1.45/deno-dom-wasm.ts";
-export * from "https://cdn.jsdelivr.net/gh/iacore/linkedom@16f8b80";
+export * from "https://esm.sh/gh/iacore/linkedom@a/esm/index.js";
